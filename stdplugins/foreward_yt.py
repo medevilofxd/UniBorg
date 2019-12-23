b7244e04aa94ac379ef0ec62b9047dc1b87ae9f4
medevilofxd <46042299+medevilofxd@users.noreply.github.com>
2019-12-23 21:45:38 +0530

    Delete foreward_yt.py

--- stdplugins/foreward_yt.py
+++
@@ -1,7 +0,0 @@
-from telethon import events
-
-@borg.on(events.NewMessage(from_users="@AudioTubeBot"))
-async def handler(event):
-    # check media type
-    if event.audio:
-    	await borg.forward_messages("@sothisismusic", event.message)
